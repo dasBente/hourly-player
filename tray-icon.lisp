70d979659aa290956185675dbb531cfdd7fbad98
Tobias Möhring <dasbente@gmail.com>
2017-11-15 22:50:52 +0100

    Build menu anew instead of reusing it

--- tray-icon.lisp
+++ tray-icon.lisp
@@ -4,19 +4,10 @@
 (defparameter *path-to-icon* "/home/dasbente/.hourlyplayer/placeholder.png")
 
 
-(let ((curr-hourly 'nil))
-  (defun update-curr-hourly (&optional register)
-    "Register a curr-hourly element or update it's label"
-    (if register
-	(setf curr-hourly register)
-	(setf (gtk-menu-item-label curr-hourly) (current-hourly)))))
-
-
 (defun build-menu ()
   "Builds a pop-up menu for the icon. Currently rebuilds the menu every time!"
   (let ((menu (gtk-menu-new)) 
-	(current-hourly (gtk-menu-item-new))
-	;; next-hourly as sub menu?
+	(current-hourly (gtk-menu-item-new-with-label (current-hourly)))
 	(next-hourly (gtk-menu-item-new-with-label "New hourly"))
 	(mute (make-instance 'gtk-check-menu-item
 			     :label "Mute"
@@ -27,9 +18,6 @@
 		      (lambda (widget)
 			(declare (ignore widget))
 			(toggle-mute)))
-
-    ;; Register current hourly item to updater
-    (update-curr-hourly current-hourly)
     
     ;; Process pressing the current hourly button
     (g-signal-connect current-hourly "activate"
@@ -59,14 +47,12 @@
    (let* ((icon (make-instance 'gtk-status-icon
 			       :pixbuf (gdk-pixbuf-new-from-file
 					*path-to-icon*)
-			      :tooltip-text "Configure hourly player"))
-	  (menu (build-menu)))
+			      :tooltip-text "Configure hourly player")))
 
      ;; Register a user clicking the icon
      (g-signal-connect icon "popup-menu"
 		       (lambda (widget button activate-time)
 			 (declare (ignore widget button activate-time))
-			 (update-curr-hourly)
-			 (gtk-menu-popup menu)))))
+			 (gtk-menu-popup (build-menu))))))
   (loop (sleep 60)))
 
