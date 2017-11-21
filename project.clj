2b0fd651f8c9075e77bc47dc210202716ba415a2
Tobias Möhring <dasbente@gmail.com>
2017-11-21 19:29:55 +0100

    Changed project description

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject hourly-player "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Tray icon and acompanying UI to the hourly player script"
+  :url "https://github.com/dasbente/hourly-player"
+  :license {:name "GPL"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot hourly-player.core
   :target-path "target/%s"
