5835d424495db1ff6b3e462bbe789afcf9720204
Tobias Möhring <dasbente@gmail.com>
2017-11-22 14:31:05 +0100

    Use clj-systemtray for tray-icon rendering

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Tray icon and acompanying UI to the hourly player script"
   :url "https://github.com/dasbente/hourly-player"
   :license {:name "GPL"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-systemtray "0.2.1"]]
   :main ^:skip-aot hourly-player.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ src/hourly_player/tray_icon.clj
@@ -0,0 +1,4 @@
+(ns hourly-player.tray-icon 
+  (:require [hourly-player.play-hourly :refer :all]
+            [clj-systemtray.core :refer :all]))
+
