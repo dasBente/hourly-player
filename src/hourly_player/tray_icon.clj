158e6262d6ddf5105e3816a21777a14ded83ed7f
Tobias Möhring <dasbente@gmail.com>
2017-11-22 21:16:21 +0100

    Working tray icon implementation (still pretty ugly though)

--- src/hourly_player/tray_icon.clj
+++ src/hourly_player/tray_icon.clj
@@ -2,3 +2,17 @@
   (:require [hourly-player.play-hourly :refer :all]
             [clj-systemtray.core :refer :all]))
 
+(defn callback 
+  "Wraps a function call into a function which ignores a callback argument"
+  [function & args] (fn [_] (apply function args)))
+
+(defn build-popup
+  "Generate a new popup consisting of a bunch of menu items"
+  [config-map]
+  (popup-menu
+   (menu-item (:current config-map) (callback play-hourly config-map))))
+
+(defn dispatch-tray
+  [config-map]
+  (make-tray-icon! (clojure.java.io/resource "placeholder.png") 
+                   (build-popup config-map)))
