fe6d460235f0f945047736fbc49d718d10fa1c12
Tobias Möhring <dasbente@gmail.com>
2017-11-22 23:18:38 +0100

    Some changes to tray-icon.clj

--- src/hourly_player/tray_icon.clj
+++ src/hourly_player/tray_icon.clj
@@ -1,18 +1,27 @@
 (ns hourly-player.tray-icon 
   (:require [hourly-player.play-hourly :refer :all]
-            [clj-systemtray.core :refer :all]))
+            [hourly-player.utils :refer :all]
+            [clj-systemtray.core :refer :all])
+  (:import [java.awt.event ActionListener]))
 
 (defn callback 
   "Wraps a function call into a function which ignores a callback argument"
   [function & args] (fn [_] (apply function args)))
 
 (defn build-popup
-  "Generate a new popup consisting of a bunch of menu items"
+  "Generate a new popup consisting of a bunch of menu items."
   [config-map]
   (popup-menu
    (menu-item (:current config-map) (callback play-hourly config-map))))
 
 (defn dispatch-tray
-  [config-map]
-  (make-tray-icon! (clojure.java.io/resource "placeholder.png") 
-                   (build-popup config-map)))
+  [callback]
+  (let [tray-icon (make-tray-icon! (clojure.java.io/resource "placeholder.png") nil)]
+    (.addActionListener tray-icon 
+                        (proxy [ActionListener] []
+                          (actionPerformed [event] (callback))))))
+
+(defn run-tray
+  []
+  (dispatch-tray (fn [] (build-popup 
+                         (read-config clojure.java.io/resource "config")))))
