ca1867ab6784fbf2e8ec9ade79c5e14fa85d6775
Tobias Möhring <dasbente@gmail.com>
2017-11-23 18:23:33 +0100

    Rework tray icon

--- src/hourly_player/tray_icon.clj
+++ src/hourly_player/tray_icon.clj
@@ -1,27 +1,64 @@
 (ns hourly-player.tray-icon 
   (:require [hourly-player.play-hourly :refer :all]
-            [hourly-player.utils :refer :all]
-            [clj-systemtray.core :refer :all])
-  (:import [java.awt.event ActionListener]))
+            [hourly-player.utils :refer :all])
+  (:import [java.awt.event ActionListener MouseListener]
+           [java.awt SystemTray TrayIcon PopupMenu MenuItem Toolkit]
+           [javax.swing JPopupMenu JMenuItem]))
 
-(defn callback 
-  "Wraps a function call into a function which ignores a callback argument"
-  [function & args] (fn [_] (apply function args)))
+(defn maybe-show-popup
+  "Takes a mouse event and a popup callback that is called when the popup was truly triggered"
+  [mouse-event popup-callback icon]
+  (when (.isPopupTrigger mouse-event)
+    (let [popup (popup-callback)]
+      (.setPopupMenu icon popup))))
 
-(defn build-popup
-  "Generate a new popup consisting of a bunch of menu items."
-  [config-map]
-  (popup-menu
-   (menu-item (:current config-map) (callback play-hourly config-map))))
+(defn updating-icon
+  "Icon that uses a builder function to create a new popup menu each time it's activated"
+  [image popup-builder]
+  (let [icon (proxy [TrayIcon] [image]
+               (getPopupMenu [] (popup-builder)))]
+    (.setImageAutoSize icon true)
+    (.setPopupMenu icon (popup-builder))
+    icon))
 
-(defn dispatch-tray
-  [callback]
-  (let [tray-icon (make-tray-icon! (clojure.java.io/resource "placeholder.png") nil)]
-    (.addActionListener tray-icon 
-                        (proxy [ActionListener] []
-                          (actionPerformed [event] (callback))))))
-
-(defn run-tray
+(defn spawn-popup-menu
+  "Create a popup menu containing nothing but a single button"
   []
-  (dispatch-tray (fn [] (build-popup 
-                         (read-config clojure.java.io/resource "config")))))
+  (let [popup (PopupMenu. "Test")
+        config (read-config config-path)]
+    (.add popup (MenuItem. (:current config)))
+    popup))
+
+(defn register-tray-icon!
+  "Get the system tray and register a given icon there"
+  [icon]
+  (let [tray (SystemTray/getSystemTray)]
+    (.add tray icon)
+    icon))
+
+(defn dispatch-tray-icon!
+  "Creates a tray icon with a given icon and optional popup building 0-ary function to build a 
+  popup on click"
+  [image popup-builder]
+  (-> image
+      (updating-icon popup-builder)
+      (register-tray-icon!)))
+
+(defn image-from-path
+  "Create a icon-ready image object from a given path"
+  [path]
+  (.getImage (Toolkit/getDefaultToolkit) path)) ;Maybe use Buffered Image?
+
+(defn run-tray-icon!
+  "Dispatches a tray icon"
+  []
+  (dispatch-tray-icon! (image-from-path (clojure.java.io/resource "placeholder.png"))
+                       spawn-popup-menu))
+
+(defn test-tray-icon!
+  "Dispatches a tray icon and removes it again after user input in the REPL"
+  []
+  (let [tray (SystemTray/getSystemTray)
+        icon (run-tray-icon!)]
+    (read)
+    (.remove tray icon)))
