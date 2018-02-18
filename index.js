245e539a35bc19c1f38d68575b225d8cec2b06a4
Tobias Möhring <dasbente@gmail.com>
2018-02-18 11:57:13 +0100

    Added click listener, prepare proceduralization of context menu

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
 const {app, Menu, Tray} = require('electron');
 const path = require('path');
 const exec = require('child_process').exec;
+const fs = require('fs');
 
-let tray, currentHourly = "Test";
+let tray;
+
+function readConfig() {
+  let res;
+
+  try {
+    res = fs.readFileSync('./resources/config.json', 'utf8');
+    res = JSON.parse(res);
+  } catch (err) {
+    console.log(`Caught {err}`);
+    res = undefined;
+  }
+  
+  return res;
+}
+
+function writeConfig(config) {
+  fs.writeFile('./resources/config.json', JSON.stringify(config), {
+    encoding: 'utf8',
+  }, (err) => {throw err;});
+}
 
 function play() {
-  exec('sh ./play-hourly Shigure 00', (error, stdout, stderr) => {
+  let currentHourly = 'Shigure', currentHour = '00';
+  exec(`sh ./play-hourly ${currentHourly} ${currentHour}`, (err, stdout, stderr) => {
     console.log(`${stdout}`);
     console.log(`${stderr}`);
     
     if (error !== null) {
-      console.log(`exec error: ${error}`);
+      console.log(`exec error: ${err}`);
     }
   });
 }
 
+let contextMenu;
+
+function buildContextMenu() {
+  return Menu.buildFromTemplate([
+    {label: 'Shigure', click: play}
+  ]);
+}
+
 app.on('ready', () => {
   tray = new Tray('./resources/icon.png');
-  
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Test', click: play},
-  ]);
-  
+  contextMenu = buildContextMenu();
   tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => {
+    play();
+  });
+
 });
+
