58bd6f780d0c9c18cb6ed5baab1f57716af9ff70
Tobias Möhring <dasbente@gmail.com>
2018-02-20 11:35:00 +0100

    Added documentation to Config.js

--- src/Config.js
+++ src/Config.js
@@ -6,23 +6,40 @@ const fs = require('fs');
 const moment = require('moment');
 const {today, hourliesFromList} = require('./player.js');
 
+/** Class representing a Config file */
 class Config {
+  /** 
+   * Create a config instance.
+   * @param {string} source - A path to the source config file
+   */
   constructor(source) {
     const conf = readConfig(source) || {};
     let t = today();
     
     this.source = source;
     this.list = conf.list? conf.list : '';
-    this.update(conf);
     this.toggleMute(conf.mute);
+    this.update(conf);
   }
 
+  /**
+   * Saves the config instance to a file.
+   * @param {string} [source] - A path to the source to be saved to. Omitting this will use the
+   *     source set in the constructor.
+   */
   save(source) {
     let src = source || this.source;
     fs.writeFileSync(src, this.toString());
   }
 
-  setHourly(hourly) {
+  /**
+   * Set the hourly to a new one by random or choice.
+   * @param {object} [opts] - A object of possible options for the hourly generation. If unset, the
+   *     hourly will just be determined at random from all hourlies saved in list this.list.
+   * @param {string} [opts.hourly] - Set this.hourly to this value.
+   * @param {stirng} [opts.list] - Set the list from which the hourly should be chosen.
+   */
+  setHourly(opts) {
     if (hourly) {
       this.current = hourly;
     } else {
@@ -33,6 +50,11 @@ class Config {
     this.today = today();
   }
 
+  /**
+   * Toggle if the app is muted.
+   * @param {string} [value] - The value to set this.mute to (out of '0' and '1'). If unset will 
+   *     merely toggle the already set value.
+   */
   toggleMute(value) {
     if (value === undefined) {
       this.mute === '0'? this.mute = '1' : this.mute = '0';
@@ -41,6 +63,11 @@ class Config {
     }
   }
 
+  /**
+   * Check if the application need to be updated.
+   * @param {Object} [config] - Initialize with values from this config if provided instead of 
+   *     regular update.
+   */
   update(config) {
     let t = today();
 
@@ -52,6 +79,9 @@ class Config {
     }
   }
 
+  /**
+   * Generates a object from this Config-instance.
+   */
   toJSON() {
     return {
       current: this.current,
@@ -61,13 +91,17 @@ class Config {
     };
   }
   
+  /**
+   * Returns stringified this.toJSON() as string representation.
+   */
   toString() {
     return JSON.stringify(this.toJSON());
   }
 }
 
 /**
- * Reads a given .JSON file and parses it if possible
+ * Parses a config file into a JS object.
+ * @param {string} confpath - Path to the config file to be parsed.
  */
 function readConfig(confpath) {
   let res;
